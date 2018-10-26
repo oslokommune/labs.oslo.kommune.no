304a9bb7273b1f93eaf452898039b5974690acd5
Erlend Schei <erlend@erlend.com>
2018-10-26 13:44:20 +0200

    Use just one webpack config. Switching modes with internal mode instead of env. Fallback to env.

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,82 @@
-module.exports = (env) => require(`./webpack.config.${env}.js`);
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env, argv) => {
+  console.log(JSON.stringify(argv, null, 2));
+  const PROD_MODE = argv.mode === 'production' || process.env.NODE_ENV === 'production';
+  return {
+    entry: {
+      main: [
+        './src/main/frontend/scripts/main.js',
+        './src/main/frontend/styles/main.scss'
+      ]
+    },
+    output: {
+      filename: PROD_MODE ? 'scripts/[name].min.js' : 'scripts/[name].js',
+      path: path.resolve(__dirname, 'src/main/resources/assets'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: ['@babel/preset-env'],
+              plugins: ["@babel/plugin-syntax-dynamic-import"]
+            }
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { 
+                sourceMap: PROD_MODE ? false : true 
+              }
+            },
+            { 
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: PROD_MODE ? false : 'inline',
+                plugins: PROD_MODE ? [
+                  require('postcss-preset-env')({
+                    autoprefixer: { 
+                      grid: true 
+                    }
+                  }),
+                  require('postcss-csso')()
+                ] : [
+                  require('postcss-preset-env')({
+                    autoprefixer: {
+                      grid: true 
+                    }
+                  })
+                ]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: { 
+                sourceMap: PROD_MODE ? false : true
+              }
+            }
+          ],
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', 'scss'],
+    },
+    stats: 'normal',
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: PROD_MODE ? 'styles/[name].min.css' : 'styles/[name].css'
+      })
+    ]
+  }
+}
