4dcfb231b4fd9cf319d774d0140edc4a9dd247ad
Jesper Kiledal <jesper@jesper.local>
2018-11-06 16:38:50 +0100

    Add (sticky) video option to hero part

--- src/main/resources/site/parts/article-list/article-list.js
+++ src/main/resources/site/parts/article-list/article-list.js
@@ -1,35 +1,33 @@
-var portal = require('/lib/xp/portal');
-var thymeleaf = require('/lib/xp/thymeleaf');
-var util = require('util');
-var cUtil = require('content-util');
-var contentLib = require('/lib/xp/content');
+var portal = require("/lib/xp/portal");
+var thymeleaf = require("/lib/xp/thymeleaf");
+var util = require("util");
+var cUtil = require("content-util");
+var contentLib = require("/lib/xp/content");
 
 exports.get = function(req) {
-    var component = portal.getComponent();
-    var config = component.config;
-    var model = {};
+  var component = portal.getComponent();
+  var config = component.config;
+  var model = {};
 
-    model.header = config.header || null;
-    
-    // Find articles
-    var siteReferencePath = portal.getContent()._path.split('/')[1];
-    var result = contentLib.query({
-        start: 0,
-        count: config.max || 100,
-        contentTypes: [
-            app.name + ':article'
-        ],
-        query: "_path LIKE '/content/" + siteReferencePath + "/*'",
-        sort: "createdTime DESC"
-    });
+  model.header = config.header || null;
 
-    // Prepare model
-    model.articles = cUtil.prepareArticleList(result, 'block(5,2)');
+  // Find articles
+  var siteReferencePath = portal.getContent()._path.split("/")[1];
+  var result = contentLib.query({
+    start: 0,
+    count: config.max || 100,
+    contentTypes: [app.name + ":article"],
+    query: "_path LIKE '/content/" + siteReferencePath + "/*'",
+    sort: "createdTime DESC"
+  });
 
-    var view = resolve('./article-list.html');
-    var body = thymeleaf.render(view, model);
-    return {
-        body: body,
-        contentType: 'text/html'
-    };
+  // Prepare model
+  model.articles = cUtil.prepareArticleList(result, "block(5,2)");
+
+  var view = resolve("./article-list.html");
+  var body = thymeleaf.render(view, model);
+  return {
+    body: body,
+    contentType: "text/html"
+  };
 };
--- src/main/resources/site/parts/hero/hero.js
+++ src/main/resources/site/parts/hero/hero.js
@@ -1,36 +1,43 @@
-var portal = require('/lib/xp/portal');
-var thymeleaf = require('/lib/xp/thymeleaf');
-var imageLib = require ('image');
-
+var portal = require("/lib/xp/portal");
+var thymeleaf = require("/lib/xp/thymeleaf");
+var imageLib = require("image");
 
 exports.get = function(req) {
-    var content = portal.getContent();
-		var model = {};
-		
-		if(content.data) {
-			
-			if(content.data.mainImage) {
-				model.image = imageLib.image.create(content.data.mainImage, 'block(5,2)');
-			}
+  var content = portal.getContent();
+  var component = portal.getComponent();
+  var model = {};
 
-			if(content.data.header) {
-				model.header = content.data.header;
-			} else {
-				model.header = content.displayName;
-			}
+  if (component.config && component.config.videoId) {
+    model.video = component.config.videoId;
 
-			if(content.data.subheader) {
-				model.subheader = content.data.subheader;
-			}
+    log.info(JSON.stringify(component.config, null, 2));
+    if (component.config.stickyVideo) {
+      model.stickyVideo = true;
+    }
+  }
 
-		}
-				
-		model.dump = content;
+  if (content.data) {
+    if (content.data.mainImage && !model.video) {
+      model.image = imageLib.image.create(content.data.mainImage, "block(5,2)");
+    }
 
-    var view = resolve('./hero.html');
-    var body = thymeleaf.render(view, model);
-    return {
-        body: body,
-        contentType: 'text/html'
-    };
+    if (content.data.header) {
+      model.header = content.data.header;
+    } else {
+      model.header = content.displayName;
+    }
+
+    if (content.data.subheader) {
+      model.subheader = content.data.subheader;
+    }
+  }
+
+  model.dump = content;
+
+  var view = resolve("./hero.html");
+  var body = thymeleaf.render(view, model);
+  return {
+    body: body,
+    contentType: "text/html"
+  };
 };
