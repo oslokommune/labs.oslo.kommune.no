dde1427d4b0c528ac72d611ccff96208b32c2271
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-03-25 12:21:06 +0100

    Add styling for category hero

--- src/main/resources/site/parts/content-image-heading-lead-body/content-image-heading-lead-body.js
+++ src/main/resources/site/parts/content-image-heading-lead-body/content-image-heading-lead-body.js
@@ -5,7 +5,7 @@ var cUtil = require('content-util')
 exports.get = function(req) {
   var content = portal.getContent()
 
-  var model = cUtil.prepareHeroContents(content.data, 'block(5,2)')
+  var model = cUtil.prepareHeroContents(content.data, 'block(5,1)')
   model.content = JSON.stringify(model, null, 2)
   var view = resolve('./content-image-heading-lead-body.html')
   var body = thymeleaf.render(view, model)
