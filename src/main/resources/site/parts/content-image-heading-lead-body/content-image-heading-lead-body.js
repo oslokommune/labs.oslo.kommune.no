55c7b91ccf927fc9e1595a45e5453156d64e3477
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-03-26 10:48:27 +0100

    Add option to hide lead on 'content-image-heading-lead-body' part

--- src/main/resources/site/parts/content-image-heading-lead-body/content-image-heading-lead-body.js
+++ src/main/resources/site/parts/content-image-heading-lead-body/content-image-heading-lead-body.js
@@ -4,9 +4,12 @@ var cUtil = require('content-util')
 
 exports.get = function(req) {
   var content = portal.getContent()
+  var component = portal.getComponent()
 
   var model = cUtil.prepareHeroContents(content.data, 'block(5,1)')
   model.content = JSON.stringify(model, null, 2)
+  model.hideLead = component && component.config && component.config.hideLead
+
   var view = resolve('./content-image-heading-lead-body.html')
   var body = thymeleaf.render(view, model)
   return {
