d2431adfea1d419b1d8be0fe70f18763e2968918
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-03-26 13:20:25 +0100

    Add content path to cache key

--- src/main/resources/site/parts/article-list/article-list.js
+++ src/main/resources/site/parts/article-list/article-list.js
@@ -10,7 +10,6 @@ var articleCache = cacheLib.newCache({
 })
 
 exports.get = function(req) {
-
   var startTime = +new Date()
 
   var content = portal.getContent()
@@ -47,57 +46,88 @@ exports.get = function(req) {
     count: config.count,
     path: content._path,
     selectedItems: config.featured,
-    contentTypes: [
-      'article'
-    ],
+    contentTypes: ['article'],
     categoryFilter: categories,
     onlyChildren: config.onlyChildren,
     paging: config.paging
   }
 
-  var result = related.getContentList(queryConfig);
+  var result = related.getContentList(queryConfig)
 
   if (result) {
     if (result.selectedHits && result.selectedHits.length) {
       // model.featuredHits = prepareData(result.selectedHits, req.mode);
-      model.featured = prepareData(result.selectedHits, scaleLandscapeFeatured, scalePortraitFeatured, req.mode, config.presentationMode)
+      model.featured = prepareData(
+        result.selectedHits,
+        scaleLandscapeFeatured,
+        scalePortraitFeatured,
+        req.mode,
+        config.presentationMode
+      )
     }
     if (result.queryHits && result.queryHits.length) {
       // model.hits = prepareData(result.queryHits, req.mode);
-      model.articles = prepareData(result.queryHits, scaleLandscape, scalePortrait, req.mode, config.presentationMode)
+      model.articles = prepareData(
+        result.queryHits,
+        scaleLandscape,
+        scalePortrait,
+        req.mode,
+        config.presentationMode
+      )
     }
     if (result.hasOwnProperty('firstPage')) {
       model.firstPage = result.firstPage
     }
     if (result.paging) {
-      model.paging = result.paging;
+      model.paging = result.paging
     }
   }
 
   model.live = req.mode == 'live'
-  model.hasContent = ((model.featured && model.featured.length) || (model.articles && model.articles.length))
+  model.hasContent =
+    (model.featured && model.featured.length) ||
+    (model.articles && model.articles.length)
   var endTime = +new Date()
-  model.controllerPageTime = 'Controller time: ' + String(endTime - startTime) + 'ms'
+  model.controllerPageTime =
+    'Controller time: ' + String(endTime - startTime) + 'ms'
   var view = resolve('./article-list.html')
   var body = thymeleaf.render(view, model)
   return {
     body: body,
     contentType: 'text/html'
   }
-
 }
 
-function prepareData(hits, scaleLandscape, scalePortrait, mode, presentationMode) {
+function prepareData(
+  hits,
+  scaleLandscape,
+  scalePortrait,
+  mode,
+  presentationMode
+) {
   return hits.map(function(resultItem) {
-    return articleCache.get(resultItem._id + resultItem.modifiedTime + scaleLandscape + scalePortrait + mode + presentationMode, function() {
-      if (resultItem.data) {
-        resultItem.data = cUtil.prepareFeaturedArticle(resultItem, scaleLandscape, scalePortrait);
-        var categories = related.getCategories(resultItem)
-        if (categories.length) {
-          resultItem.data.categories = categories
+    return articleCache.get(
+      resultItem._id +
+        resultItem._path +
+        resultItem.modifiedTime +
+        scaleLandscape +
+        scalePortrait +
+        mode +
+        presentationMode,
+      function() {
+        if (resultItem.data) {
+          resultItem.data = cUtil.prepareFeaturedArticle(
+            resultItem,
+            scaleLandscape,
+            scalePortrait
+          )
+          var categories = related.getCategories(resultItem)
+          if (categories.length) {
+            resultItem.data.categories = categories
+          }
         }
+        return resultItem.data
       }
-      return resultItem.data
-    })
+    )
   })
 }
