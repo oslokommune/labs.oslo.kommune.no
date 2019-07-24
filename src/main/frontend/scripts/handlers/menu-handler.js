24cacca7cea722b9002f3f4977485ee002c21684
Erlend Schei <erlend@erlend.com>
2019-07-24 19:01:28 +0100

    Menu handling rewritten to handle closing on outside clicks and esc

--- src/main/frontend/scripts/handlers/menu-handler.js
+++ src/main/frontend/scripts/handlers/menu-handler.js
@@ -8,20 +8,58 @@ export default function init() {
 
 function togglemenu(e) {
   e.preventDefault()
-
-  if (Array.from(this.classList).includes('is-open')) {
-    menuButton.classList.remove('is-open')
-    menuDrawer.classList.remove('is-open')
-    headerElement.classList.remove('is-open')
-
-    menuButton.setAttribute('aria-label', ariaLabelOpenMenu) // Global var from html
-    menuButton.setAttribute('aria-expanded', false)
+  if (Array.from(menuButton.classList).includes('is-open')) {
+    closeMenu()
   } else {
-    menuButton.classList.add('is-open')
-    menuDrawer.classList.add('is-open')
-    headerElement.classList.add('is-open', 'header--pin')
+    e.stopPropagation()
+    openMenu()
+  }
+}
 
-    menuButton.setAttribute('aria-label', ariaLabelCloseMenu)
-    menuButton.setAttribute('aria-expanded', true)
+function closeMenu() {
+  menuButton.classList.remove('is-open')
+  menuDrawer.classList.remove('is-open')
+  headerElement.classList.remove('is-open')
+
+  menuButton.setAttribute('aria-label', ariaLabelOpenMenu) // Global var from html
+  menuButton.setAttribute('aria-expanded', false)
+
+  // Remove handler for outside clicks and escape
+  document.removeEventListener('click', handleClick)
+  document.removeEventListener('keydown', handleKeydown)
+}
+
+function openMenu() {
+  menuButton.classList.add('is-open')
+  menuDrawer.classList.add('is-open')
+  headerElement.classList.add('is-open', 'header--pin')
+
+  menuButton.setAttribute('aria-label', ariaLabelCloseMenu)
+  menuButton.setAttribute('aria-expanded', true)
+
+  // Add handler for outside clicks and escape
+  document.addEventListener('click', handleClick)
+  document.addEventListener('keydown', handleKeydown)
+}
+
+function handleClick(e) {
+  var targetElement = e.target
+  // Follow DOM to check if click was inside menu drawer
+  do {
+    if (targetElement == menuDrawer) {
+      return
+    }
+    targetElement = targetElement.parentNode
+  } while (targetElement)
+
+  // This is a click outside
+  // e.preventDefault()
+  // e.stopPropagation()
+  closeMenu()
+}
+
+function handleKeydown(e) {
+  if (27 === e.keyCode) {
+    closeMenu()
   }
 }
