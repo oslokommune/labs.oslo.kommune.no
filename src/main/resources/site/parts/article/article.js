23c162fbf64482c3e2c896811574f8a055a30ec8
Erlend Schei <erlend@erlend.com>
2020-11-05 13:48:55 +0100

    Enabled override of article in article part, to use on landing pages

--- src/main/resources/site/parts/article/article.js
+++ src/main/resources/site/parts/article/article.js
@@ -1,11 +1,23 @@
 var portal = require('/lib/xp/portal')
+var contentLib = require('/lib/xp/content')
 var thymeleaf = require('/lib/thymeleaf')
 var util = require('/lib/labs/util.js')
 var contentPrep = require('/lib/labs/content-prep.js')
 var related = require('/lib/labs/related.js')
 
 exports.get = function(req) {
-  var content = portal.getContent()
+  var content
+  var component = portal.getComponent()
+  var config = component.config
+  if (config && config.override) {
+    // if hardcoded content link
+    content = contentLib.get({
+      key: component.config.override,
+    })
+  } else {
+    // get default content for this url
+    content = portal.getContent()
+  }
 
   // Move publish metadata to data
   content.createdTime && (content.data.createdTime = content.createdTime)
@@ -23,6 +35,6 @@ exports.get = function(req) {
   var body = thymeleaf.render(view, model)
   return {
     body: body,
-    contentType: 'text/html'
+    contentType: 'text/html',
   }
 }
