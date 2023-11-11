96a95261fab5778ad9ce99d6fd98ab7df0108487
Erlend Schei <erlend@erlend.com>
2023-11-11 15:34:35 +0100

    Start work on video part

---
+++ src/main/resources/site/parts/video/video.js
@@ -0,0 +1,40 @@
+var portal = require('/lib/xp/portal')
+var thymeleaf = require('/lib/thymeleaf')
+var contentLib = require('/lib/xp/content')
+var contentPrep = require('/lib/labs/content-prep.js')
+
+exports.get = function(req) {
+  var content = portal.getContent()
+
+  var model = {}
+
+  var query = "_path LIKE '/content/*'"
+  var result = contentLib.query({
+    start: 0,
+    count: 20,
+    query: query,
+    contentTypes: [app.name + ':article'],
+    sort: 'createdTime DESC',
+    filters: {
+      hasValue: {
+        field: 'data.authors',
+        values: [content._id]
+      }
+    }
+  })
+
+  model.articles = contentPrep.prepareArticleList(result, 'block(4,3)')
+
+  if (content.data && content.data.name) {
+    model.name = content.data.name
+  } else {
+    model.name = content.displayName
+  }
+
+  var view = resolve('./person.html')
+  var body = thymeleaf.render(view, model)
+  return {
+    body: body,
+    contentType: 'text/html'
+  }
+}
