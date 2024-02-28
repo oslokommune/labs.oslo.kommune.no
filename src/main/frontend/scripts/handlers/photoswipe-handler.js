0e9aedb14657cba96795c9e233a97dda65f1a1f0
Erlend Schei <erlend@erlend.com>
2024-02-28 13:10:41 +0100

    Delete leftovers from photoswipe 4. Improve spacing on mobile.

--- src/main/frontend/scripts/handlers/photoswipe-handler.js
+++ src/main/frontend/scripts/handlers/photoswipe-handler.js
@@ -7,12 +7,12 @@ export default function init() {
     children: '.gallery',
     pswpModule: () => import('../../../../../node_modules/photoswipe/dist/photoswipe.esm.js'),
     paddingFn: (viewportSize) => {
-      return {
-        top: 30,
-        bottom: 30,
-        left: 70,
-        right: 70,
-      }
+      const configurations = [
+        { max: 600, values: { top: 0, right: 0, bottom: 0, left: 0 } },
+        { max: 1024, values: { top: 10, right: 10, bottom: 10, left: 10 } },
+        { max: Infinity, values: { top: 30, right: 70, bottom: 30, left: 70 } },
+      ]
+      return configurations.find((config) => viewportSize.x < config.max).values
     },
   })
 
